aea0e043aa46d1298bb880b2cbfa953f05fb8819
Steven Andrew Smith <me@igsteven.com>
2022-06-20 14:40:28 +0100

    Sample WHOIS works
    This is a sample of whois working, Now to completely break it, making it use object instead of just spitting out the results.

--- example/getAS.js
+++ example/getAS.js
@@ -1,4 +1,25 @@
-const RIPE = require('./../')
+/* 
+    RIPE DB Client, A NodeJS Client for Ripe DB.
+
+    Copyright 2022, Steven Andrew Smith, All Rights Reserved.
+    License: GNU GENERAL PUBLIC LICENSE v3.0
+
+    Wiki: https://github.com/IGSteven/RIPE.js/wiki
+    License: https://github.com/IGSteven/RIPE.js/blob/main/LICENSE
+    Issues: https://github.com/IGSteven/RIPE.js/issues
+*/ 
+
+// Load the Requirements 
+const RIPE = require('./../') // Note this will be diffrent for you.
 const client = new RIPE();
 
-client.test();
+// This will only work within a Asynchronous Eviroment
+const main = async ()=>{
+
+    // We now can run the .whois() function to find the Autonomous System (AS)
+    let AS = await client.whois('as42831') // In this example, We are using UKSERVERS-AS.
+    console.log(JSON.stringify(AS));
+
+}
+
+main()
--- index.js
+++ index.js
@@ -4,14 +4,14 @@
     Copyright 2022, Steven Andrew Smith, All Rights Reserved.
     License: GNU GENERAL PUBLIC LICENSE v3.0
 
-    WIKI: https://github.com/IGSteven/RIPE.js/wiki
+    Wiki: https://github.com/IGSteven/RIPE.js/wiki
     License: https://github.com/IGSteven/RIPE.js/blob/main/LICENSE
     Issues: https://github.com/IGSteven/RIPE.js/issues
 */ 
 
 // Requirements
 const EventEmitter = require('events');
-const axios = require('axios');
+const axios = require('axios').default;
 
 // Client Class
 class client extends EventEmitter {
@@ -22,19 +22,26 @@ class client extends EventEmitter {
         token = null // We define this by doing login()
     }
 
-    query = async function (t="whois", p={}) {
-        // Identify Type and Build Request.
-        var request = {};  
-        switch(t.toLowerCase()){
-            case "whois": request = { url: 'https://stat.ripe.net/data/whois/data.json', method: 'get', params: { resourse: p }};
-            default: {
-                try {
-                    if (request === {}) break;
-                    let response = await axios(request)
-                    return(response);
-                } catch (error) { console.log(error) }
-            } 
+    query = async (type="whois", params={}) => {
+        var request = {
+            url: `https://stat.ripe.net/data/${type}/data.json`,
+            method: 'get',
+            params: params,
         }
+        request.params.sourceapp = "IGStevensRIPEJS"; // Add Source App to params
+        
+        try {
+            const res = await axios(request);
+            //console.log(res.data)
+            return res.data.data;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
+    whois = async function(who) {
+        return this.query('whois', { resource: who });
     }
 
     login = async function (input){
@@ -44,7 +51,7 @@ class client extends EventEmitter {
     }
 
     test = async function () {
-        let r = this.query('whois', "as42831");
+        let r = this.query('whois', {resource: "as42831"});
         console.log(r)
     }
 }
